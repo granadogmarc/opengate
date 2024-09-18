6376a4a31693a4964787aa08be6b447911cd94b7
Thomas BAUDIER <thomas.baudier@creatis.insa-lyon.fr>
2024-09-18 11:04:39 +0200

    Prepare qt6 for linux
    To be able to use qt6 I need gcc version quite recent, ie. I need to use
    manylinux_2_28.
    But for people/cluster using older OS, I continue to compile the wheel
    with manylinux2014 only for tagged version.

--- .github/workflows/createWheelLinux.sh
+++ .github/workflows/createWheelLinux.sh
@@ -7,11 +7,12 @@ export PATH=/software/cmake/cmake-3.18.2-Linux-x86_64/bin/:${PATH}
 source /software/geant4/bin/geant4make.sh
 export CMAKE_PREFIX_PATH=/software/geant4/bin:/software/itk/bin/:${CMAKE_PREFIX_PATH}
 mkdir opengate_core/plugins
-cp -r /lib64/qt5/plugins/platforms opengate_core/plugins
-cp -r /lib64/qt5/plugins/imageformats opengate_core/plugins
+cp -r /opt/Qt/6.7.2/gcc_64/plugins/platforms opengate_core/plugins/
+cp -r /opt/Qt/6.7.2/gcc_64/plugins/imageformats opengate_core/plugins/
+cp -r /opt/Qt/6.7.2/gcc_64/plugins/xcbglintegrations opengate_core/plugins/
 /opt/python/${PYTHONFOLDER}/bin/pip install wget colored
 /opt/python/${PYTHONFOLDER}/bin/python setup.py sdist bdist_wheel
-auditwheel repair /home/core/dist/*.whl -w /software/wheelhouse/ --plat "manylinux2014_x86_64"
+auditwheel repair /home/core/dist/*.whl -w /software/wheelhouse/ --plat "manylinux_2_28_x86_64"
 cp -r /software/wheelhouse /home/
 #/opt/python/${PYTHONFOLDER}/bin/pip install twine
 #/opt/python/${PYTHONFOLDER}/bin/twine upload --repository-url https://test.pypi.org/legacy/ wheelhouse/*manylinux2014*.whl
