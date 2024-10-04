7b80c047f537df497813ebef28752fddb2717b76
Thomas BAUDIER <thomas.baudier@creatis.insa-lyon.fr>
2024-10-04 16:11:55 +0200

    Try to debug qt6

--- .github/workflows/createWheelLinux.sh
+++ .github/workflows/createWheelLinux.sh
@@ -8,15 +8,15 @@ source /software/geant4/bin/geant4make.sh
 export CMAKE_PREFIX_PATH=/software/geant4/bin:/software/itk/bin/:${CMAKE_PREFIX_PATH}
 mkdir opengate_core/plugins
 ls /opt/Qt/6.7.2/gcc_64/plugins/
-#cp -r /opt/Qt/6.7.2/gcc_64/plugins/designer/ opengate_core/plugins/
+cp -r /opt/Qt/6.7.2/gcc_64/plugins/designer/ opengate_core/plugins/
 #cp -r /opt/Qt/6.7.2/gcc_64/plugins/egldeviceintegrations/ opengate_core/plugins/
 #cp -r /opt/Qt/6.7.2/gcc_64/plugins/generic/ opengate_core/plugins/
 #cp -r /opt/Qt/6.7.2/gcc_64/plugins/iconengines/ opengate_core/plugins/
 cp -r /opt/Qt/6.7.2/gcc_64/plugins/imageformats opengate_core/plugins/
 #cp -r /opt/Qt/6.7.2/gcc_64/plugins/networkinformation/ opengate_core/plugins/
-##cp -r /opt/Qt/6.7.2/gcc_64/plugins/platforminputcontexts/ opengate_core/plugins/
+##cp -r /opt/Qt/6.7.2/gcc_64/plugins/platforminputcontexts/ opengate_core/plugins/ #libpango-1.0.so.0
 cp -r /opt/Qt/6.7.2/gcc_64/plugins/platforms opengate_core/plugins/
-cp -r /opt/Qt/6.7.2/gcc_64/plugins/platformthemes opengate_core/plugins/
+##cp -r /opt/Qt/6.7.2/gcc_64/plugins/platformthemes opengate_core/plugins/ #libgtk-3.so.0
 #cp -r /opt/Qt/6.7.2/gcc_64/plugins/qmllint opengate_core/plugins/
 #cp -r /opt/Qt/6.7.2/gcc_64/plugins/qmltooling opengate_core/plugins/
 #cp -r /opt/Qt/6.7.2/gcc_64/plugins/sqldrivers opengate_core/plugins/
