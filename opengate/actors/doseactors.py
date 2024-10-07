043abacbc5bae084fbdbb3d407bf22074c69cfbd
Alexis Pereda <alexis.pereda@clermont.in2p3.fr>
2024-10-07 12:23:17 +0200

    [biodose] add alphamix and sqrtbetamix

--- opengate/actors/doseactors.py
+++ opengate/actors/doseactors.py
@@ -1053,7 +1053,9 @@ class BioDoseActor(VoxelDepositActor, g4.GateBioDoseActor):
 
     """
 
-    type_name = "BioDoseActor"
+    # hints for IDE
+    cell_line: str
+    biophysical_model: str
 
     user_info_defaults = {
         "cell_line": (
@@ -1095,6 +1097,22 @@ class BioDoseActor(VoxelDepositActor, g4.GateBioDoseActor):
         )
         self.user_output.biodose.set_item_suffix("dose", item=1)
 
+        self._add_interface_to_user_output(
+            UserInterfaceToActorOutputImage,
+            "biodose",
+            "alphamix",
+            item=2,
+        )
+        self.user_output.biodose.set_item_suffix("alphamix", item=2)
+
+        self._add_interface_to_user_output(
+            UserInterfaceToActorOutputImage,
+            "biodose",
+            "sqrtbetamix",
+            item=3,
+        )
+        self.user_output.biodose.set_item_suffix("sqrtbetamix", item=3)
+
         self.__initcpp__()
         self.__finalize_init__()
 
@@ -1135,6 +1153,8 @@ class BioDoseActor(VoxelDepositActor, g4.GateBioDoseActor):
             run_index,
             self.cpp_edep_image,
             self.cpp_dose_image,
+            self.cpp_alphamix_image,
+            self.cpp_sqrtbetamix_image,
         )
 
         g4.GateBioDoseActor.BeginOfRunActionMasterThread(self, run_index)
@@ -1145,6 +1165,8 @@ class BioDoseActor(VoxelDepositActor, g4.GateBioDoseActor):
             run_index,
             self.cpp_edep_image,
             self.cpp_dose_image,
+            self.cpp_alphamix_image,
+            self.cpp_sqrtbetamix_image,
         )
         self._update_output_coordinate_system("biodose", run_index)
 
