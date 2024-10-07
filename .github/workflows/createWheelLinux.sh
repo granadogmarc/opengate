7e5f8a6c20f905aab472e049eebbe4a607a5eeb6
Thomas BAUDIER <thomas.baudier@creatis.insa-lyon.fr>
2024-10-07 10:51:22 +0200

    Try to debug qt6

--- .github/workflows/createWheelLinux.sh
+++ .github/workflows/createWheelLinux.sh
@@ -10,18 +10,18 @@ mkdir opengate_core/plugins
 ls /opt/Qt/6.7.2/gcc_64/plugins/
 cp -r /opt/Qt/6.7.2/gcc_64/plugins/designer/ opengate_core/plugins/ #pareil
 cp -r /opt/Qt/6.7.2/gcc_64/plugins/egldeviceintegrations/ opengate_core/plugins/ #pareil
-cp -r /opt/Qt/6.7.2/gcc_64/plugins/generic/ opengate_core/plugins/
-#cp -r /opt/Qt/6.7.2/gcc_64/plugins/iconengines/ opengate_core/plugins/
-cp -r /opt/Qt/6.7.2/gcc_64/plugins/imageformats opengate_core/plugins/
+cp -r /opt/Qt/6.7.2/gcc_64/plugins/generic/ opengate_core/plugins/ #pareil
+cp -r /opt/Qt/6.7.2/gcc_64/plugins/iconengines/ opengate_core/plugins/
+cp -r /opt/Qt/6.7.2/gcc_64/plugins/imageformats opengate_core/plugins/ #initial
 #cp -r /opt/Qt/6.7.2/gcc_64/plugins/networkinformation/ opengate_core/plugins/
 ##cp -r /opt/Qt/6.7.2/gcc_64/plugins/platforminputcontexts/ opengate_core/plugins/ #libpango-1.0.so.0
-cp -r /opt/Qt/6.7.2/gcc_64/plugins/platforms opengate_core/plugins/
+cp -r /opt/Qt/6.7.2/gcc_64/plugins/platforms opengate_core/plugins/ #initial
 ##cp -r /opt/Qt/6.7.2/gcc_64/plugins/platformthemes opengate_core/plugins/ #libgtk-3.so.0
 #cp -r /opt/Qt/6.7.2/gcc_64/plugins/qmllint opengate_core/plugins/
 #cp -r /opt/Qt/6.7.2/gcc_64/plugins/qmltooling opengate_core/plugins/
 #cp -r /opt/Qt/6.7.2/gcc_64/plugins/sqldrivers opengate_core/plugins/
 #cp -r /opt/Qt/6.7.2/gcc_64/plugins/tls opengate_core/plugins/
-cp -r /opt/Qt/6.7.2/gcc_64/plugins/xcbglintegrations opengate_core/plugins/
+cp -r /opt/Qt/6.7.2/gcc_64/plugins/xcbglintegrations opengate_core/plugins/ #initial
 /opt/python/${PYTHONFOLDER}/bin/pip install wget colored
 /opt/python/${PYTHONFOLDER}/bin/python setup.py sdist bdist_wheel
 auditwheel repair /home/core/dist/*.whl -w /software/wheelhouse/ --plat "manylinux_2_28_x86_64"
